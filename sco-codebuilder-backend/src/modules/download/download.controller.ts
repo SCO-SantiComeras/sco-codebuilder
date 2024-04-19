e3e46f063d099bb046a7220e52c664dbcdf3dd14
scomeras94 <scomeras@gmail.com>
2024-04-19 19:26:58 +0200

    ADD - Download folder controller

--- sco-codebuilder-backend/src/modules/download/download.controller.ts
+++ sco-codebuilder-backend/src/modules/download/download.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { httpErrorMessages } from 'src/constants/http-error-messages.constants';
+import { Controller, Get, HttpException, HttpStatus, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { DownloadService } from './download.service';
 import { DownloadDto } from './dto/download.dto';
@@ -13,8 +14,24 @@ export class DownloadController {
     @Res() res: Response, 
     @Param('folder') folder: string,
   ): Promise<Response<DownloadDto, Record<string, DownloadDto>>> {
+    const existPath: boolean = await this.downloadService.exists(folder);
+    if (!existPath) {
+      console.log(`[downloadFolder] Folder '${folder}' not exists`);
+      throw new HttpException(httpErrorMessages.DOWNLOAD.DOWNLOAD_FOLDER_NOT_EXISTS, HttpStatus.CONFLICT);
+    }
 
-    const downloadDto: DownloadDto = await this.downloadService.downloadFolder(folder);
+    const files: string[] = await this.downloadService.listFiles(folder);
+    if (!files || (files && files.length == 0)) {
+      console.log(`[downloadFolder] Folder '${folder}' not cotains files`);
+      throw new HttpException(httpErrorMessages.DOWNLOAD.DOWNLOAD_FOLDER_NO_FILES, HttpStatus.CONFLICT);
+    }
+
+    const downloadDto: DownloadDto = await this.downloadService.downloadFolder(folder, files);
+    if (!downloadDto) {
+      console.log(`[downloadFolder] Folder '${folder}' unnable to create download file`);
+      throw new HttpException(httpErrorMessages.DOWNLOAD.UNNABLE_CREATE_DOWNLOAD_FILE, HttpStatus.CONFLICT);
+    }
+    
     return res.status(200).json(downloadDto);
   }
 }
